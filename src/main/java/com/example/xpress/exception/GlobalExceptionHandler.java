fc82022bdbb25f8a0b267155c3a97fad3a0844fd
Wendel Isaac <wendel.isaac12@gmail.com>
2024-10-11 12:47:36 -0300

    feat: global class to handle the exceptions

---
+++ src/main/java/com/example/xpress/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.xpress.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
