0f26a72e2f0412bfccf448e5fe051e3335a42899
Wendel Isaac <wendel.isaac12@gmail.com>
2024-10-11 15:03:21 -0300

    feat: category controller

---
+++ src/main/java/com/example/xpress/controllers/CategoryController.java
@@ -0,0 +1,50 @@
+package com.example.xpress.controllers;
+
+import com.example.xpress.entities.Category;
+import com.example.xpress.entities.Product;
+import com.example.xpress.repository.CategoryRepository;
+import com.example.xpress.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @GetMapping
+    public List<Category> categories(){
+        List<Category> results = categoryRepository.findAll();
+        return results;
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id){
+        Category results = categoryRepository.findById(id).get();
+        return results;
+    }
+
+    @GetMapping("/{name}/products")
+    public List<Product> getProductsByCategory(@PathVariable String name){
+        List<Product> results = productRepository.findByCategoryName(name);
+        return results;
+    }
+
+    @PostMapping
+    public Category addCategorie(@RequestBody Category category){
+        Category results = categoryRepository.save(category);
+        return results;
+    }
+
+    @DeleteMapping
+    public void deleteCategoryById(@RequestParam Long id){
+        categoryRepository.deleteById(id);
+    }
+}
