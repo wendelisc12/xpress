4a37ee9c6dbf003ac114a740be26b278ccf440b0
Wendel Isaac <wendel.isaac12@gmail.com>
2024-10-17 14:16:08 -0300

    feat: function delete payment

--- src/main/java/com/example/xpress/controllers/PaymentController.java
+++ src/main/java/com/example/xpress/controllers/PaymentController.java
@@ -27,4 +27,8 @@ public class PaymentController {
         return results;
     }
 
+    @DeleteMapping
+    public void deletePayment(@RequestParam Long id){
+        paymentRepository.deleteById(id);
+    }
 }
