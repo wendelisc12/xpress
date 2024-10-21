89f5af585083606289f84e33db0d0b38208028e3
Wendel Isaac <wendel.isaac12@gmail.com>
2024-10-21 11:14:38 -0300

    feat: add class for custom error message

---
+++ src/main/java/com/example/xpress/exception/RestErrorMessage.java
@@ -0,0 +1,29 @@
+package com.example.xpress.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class RestErrorMessage {
+    private HttpStatus status;
+    private String message;
+
+    public RestErrorMessage(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
