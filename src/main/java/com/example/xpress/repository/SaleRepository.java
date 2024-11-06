0a9e2ee87e7c3b47120b7731cf39854d297c5de2
wendelisc <w.lopes.da.penha@accenture.com>
2024-11-06 15:12:14 -0300

    chore: receving a string instead a Long in id sale

--- src/main/java/com/example/xpress/repository/SaleRepository.java
+++ src/main/java/com/example/xpress/repository/SaleRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface SaleRepository extends JpaRepository<Sale, Long> {
+public interface SaleRepository extends JpaRepository<Sale, String> {
     List<Sale> findByPaymentId(Long id);
 }
